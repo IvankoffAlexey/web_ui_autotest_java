a529d3a9cc8c38c8beb75e12dd8544be458b4dac
IvankoffAlexey <alexey.ivankoff@gmail.com>
2022-03-31 12:52:48 +0300

    Добавлен практический материал разобранный на третьем уроке.

---
+++ src/main/java/ru/gb/lesson3/SetupDriverExamples.java
@@ -0,0 +1,39 @@
+package ru.gb.lesson3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SetupDriverExamples {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-notifications");
+        //chromeOptions.addArguments("user-agent=Googlebot/2.1 (+http://www.google.com/bot.html)");
+        //chromeOptions.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://google.com");
+        Thread.sleep(3000);
+
+        //((JavascriptExecutor)driver).executeScript("window.open()");
+        //driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        List<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        Thread.sleep(2000);
+        driver.close();
+
+
+        Thread.sleep(2000);
+
+        driver.quit();
+    }
+}
