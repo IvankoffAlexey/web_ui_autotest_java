f26423618ebb6f4021d075e4e992979e9661054e
IvankoffAlexey <alexey.ivankoff@gmail.com>
2022-04-12 12:09:50 +0300

    Исправлены XPath локаторы в ПЗ к третьему уроку.

--- src/main/java/ru/gb/lesson3/homework/seleniumIde/AddToCartTest.java
+++ src/main/java/ru/gb/lesson3/homework/seleniumIde/AddToCartTest.java
@@ -13,26 +13,26 @@ public class AddToCartTest {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         driver.get("http://automationpractice.com/index.php");
         driver.manage().window().setSize(new Dimension(1048, 1650));
-        driver.findElement(By.xpath("//a[@title=\"Log in to your customer account\"]")).click();
+        driver.findElement(By.xpath("//a[@title=\'Log in to your customer account\']")).click();
         driver.findElement(By.id("email")).click();
         driver.findElement(By.id("email")).sendKeys("qatesta@mail.ru");
         driver.findElement(By.id("passwd")).sendKeys("1q2w3e4r5t");
         driver.findElement(By.id("SubmitLogin")).click();
-        driver.findElement(By.xpath("//*[@alt=\"My Store\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[1]/div/div[2]/div[2]/a[1]")).click();
-        driver.findElement(By.xpath("//*[@title=\"Continue shopping\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[2]/div/div[2]/div[2]/a[1]")).click();
+        driver.findElement(By.xpath("//*[@alt=\'My Store\']")).click();
+        driver.findElement(By.xpath("//a[@data-id-product='1']")).click();
+        driver.findElement(By.xpath("//*[@title=\'Continue shopping\']")).click();
+        driver.findElement(By.xpath("//a[@data-id-product='2']")).click();
         driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/span")).click();
-        driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[3]/div/div[2]/div[2]/a[1]")).click();
-        driver.findElement(By.xpath("//a[@title=\"Proceed to checkout\"]")).click();
+        driver.findElement(By.xpath("//a[@data-id-product='3']")).click();
+        driver.findElement(By.xpath("//*[@title=\'Proceed to checkout\']")).click();
         driver.findElement(By.id("3_13_0_670399")).click();
         driver.findElement(By.id("2_7_0_670399")).click();
         driver.findElement(By.id("1_1_0_670399")).click();
-        driver.findElement(By.xpath("//a[@title=\"Log me out\"]")).click();
+        driver.findElement(By.xpath("//a[@title=\'Log me out\']")).click();
         driver.navigate().refresh();
         driver.close();
     }
